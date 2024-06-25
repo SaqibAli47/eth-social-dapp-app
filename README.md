2c540934bb745282d6bc2edc94041e9315934ad3
SaqibAli47 <bsit2017047@student.gudgk.edu.pk>
2024-06-25 11:47:05 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Eth Social Dapp App
+
+A simple social media dapp app built on the Ethereum blockchain, which allow user to create post as an NFT, view posts and purchase posts.
+
+> Note: The app is not deployed to any testnet or mainnet, it is only running on a local blockchain for the purpose of learning. Do not use it to store any real ETH or NFTs.
+
+
+## Getting Started
+
+- Install [Ganache](https://trufflesuite.com/ganache/) for running a private blockchain
+  - In Ganache, create a new network, remember the port.
+- Install Metamask or an EVM compatible wallet, enable/add the network created in Ganache. (something like http://localhost:7545)
+- Run `npm install` to install dependencies.
+- Run `npm run migrate` to compile and deploy the smart contracts to the local blockchain.
+
+## Development
+
+### Remember to run `npm run migrate` after making changes to the smart contracts.
+
+### Send ETH to your wallet
+
+- In Ganache **Accounts** tab, copy one of the public key that starts with `0x` as a sender. Remember to choose the one that has some ETH.
+- In Metamask, copy the public key of the account that you want to send ETH to.
+- Run `npm run console` to open the truffle console.
+- Run `web3.eth.sendTransaction({from: '0x_sender_address', to: '0x_receiver_address', value: web3.utils.toWei('amount_to_send')})`
+- Go back to your wallet and switch to the Localhost network, you should see the ETH balance updated.
+
+### Link Truffle to Ganache
+- In Ganache running serer, open **Contracts** -> Add Project -> Select the `truffle-config.js` file in the project folder.
+- You will see the list of contracts that are compiled and which has been deployed to the Ganache blockchain.
+
+## Troubleshooting
+### Wallet popup not showing up
+- Make sure you have a browser wallet installed and enabled.
+- Make sure you allow popups from the site.
---
+++ src/app/page.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import useGetPosts, { Post, connectWallet } from '../hooks/use-get-posts';
+import { shortenAddress } from '../utils/shorten-address';
+
+export default function Home() {
+  const { posts, createPost, buyPost } = useGetPosts();
+  const [input, setInput] = useState("");
+  const [price, setPrice] = useState("");
+  const [connectedAddress, setConnectedAddress] = useState('');
+
+  const checkConnection = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      return;
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    if (accounts.length > 0) {
+      setConnectedAddress(accounts[0]);
+    } else {
+      setConnectedAddress('');
+    }
+  };
+
+  useEffect(() => {
+    checkConnection();
+  }, []);
+
+  if (!posts) {
+    return <div />;
+  }
+
+  function sendPost() {
+    if (!input || !price) return;
+    createPost(input, price);
+    setInput("");
+    setPrice("");
+  }
+
+  return (
+    <main className="bg-white h-full p-5 flex flex-col gap-10">
+      <div className='flex flex-col gap-10'>
+        <div className='flex justify-between align-center'>
+          <h1 className='block font-sans text-5xl font-semibold'>ETH Social Dapp</h1>
+          {connectedAddress ? (
+            <p className='block font-sans text-xl'>Connected to: {shortenAddress(connectedAddress)}</p>
+
+          ) : (
+            <button className="rounded-md bg-green-300 py-1 px-2 text-center" onClick={connectWallet}>
+              Login
+            </button>
+          )}
+        </div>
+        <div className='flex flex-col gap-4 max-w-md'>
+          <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3" placeholder='Content' value={input} onChange={(e) => setInput(e.target.value)} />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3" type="number" placeholder='Price' value={price} onChange={(e) => setPrice(e.target.value)} />
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={sendPost}>
+            Post
+          </button>
+        </div>
+      </div>
+      <div className='flex flex-wrap gap-4 p-1 w-full'>
+        {posts.map((post: Post, index: number) => (
+          <div key={post.id} className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border bg-gray-100 p-3 gap-8">
+            <div>
+              <p className="block font-sans text-base">
+                {post.text}
+              </p>
+              <hr className='my-2' />
+              {post.owner === post.originalOwner ? (
+                <>
+                  <p className="text-gray-500">Owner & Author: {shortenAddress(post.owner)}</p>
+                  <p className="text-gray-500">Never sold</p>
+                </>
+              ) : (
+                <>
+                  <p className="text-gray-500">Owner: {shortenAddress(post.owner)}</p>
+                  <p className="text-gray-500">Original Owner: {shortenAddress(post.originalOwner)}</p>
+                </>
+              )}
+            </div>
+
+            <div className="flex justify-between align-center">
+              <button className="rounded-md bg-green-300 py-1 px-2 text-center" onClick={() => buyPost(index)}>
+                Buy
+              </button>
+              <strong className="block font-sans text-base">
+                {`${ethers.formatEther(post.price)} ETH`}
+              </strong>
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
